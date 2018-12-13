7e69bfac964194f33b7c60ef2892b2959fae951f
WareJosephB <warejosephb@gmail.com>
2018-12-13 14:32:22 +0000

    Refactored to allow for queue requests

--- src/main/java/com/qa/TrainerAPI/service/business/TrainerServiceImpl.java
+++ src/main/java/com/qa/TrainerAPI/service/business/TrainerServiceImpl.java
@@ -3,7 +3,6 @@ package com.qa.TrainerAPI.service.business;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
 
 import com.qa.TrainerAPI.util.TrainerProducer;
@@ -11,7 +10,6 @@ import com.qa.persistence.domain.Trainer;
 import com.qa.persistence.domain.User;
 import com.qa.persistence.domain.UserRequest;
 import com.qa.persistence.domain.UserRequest.requestType;
-import com.qa.TrainerAPI.util.Constants;
 
 @Component
 public class TrainerServiceImpl implements TrainerService {
@@ -19,38 +17,31 @@ public class TrainerServiceImpl implements TrainerService {
 	@Autowired
 	private TrainerProducer producer;
 
-	@Autowired
-	private JmsTemplate jmsTemplate;
-
 	public String create(Trainer trainer) {
 		trainer.setType("Trainer");
 		UserRequest thisRequest = new UserRequest(trainer);
 		thisRequest.setHowToAct(requestType.CREATE);
-		producer.produce(thisRequest);
-		return Constants.TRAINER_QUEUED_MESSAGE;
+		return producer.produceMessage(thisRequest);
 	}
 
 	public Optional<Trainer> get(String userName) {
 		User dummyUser = new Trainer(userName);
 		UserRequest thisRequest = new UserRequest(dummyUser);
 		thisRequest.setHowToAct(requestType.READ);
-		producer.produce(thisRequest);
-		return (Optional<Trainer>) jmsTemplate.receiveAndConvert(Constants.OUTGOING_TRAINER_QUEUE_NAME);
+		return producer.produceTrainer(thisRequest);
 	}
 
 	public Iterable<Trainer> getAll() {
 		UserRequest thisRequest = new UserRequest();
 		thisRequest.setHowToAct(requestType.READALL);
-		producer.produce(thisRequest);
-		return (Iterable<Trainer>) jmsTemplate.receiveAndConvert(Constants.OUTGOING_TRAINER_QUEUE_NAME);
+		return producer.produceTrainers(thisRequest);
 	}
 
 	public String delete(String userName) {
 		User dummyUser = new Trainer(userName);
 		UserRequest thisRequest = new UserRequest(dummyUser);
 		thisRequest.setHowToAct(requestType.DELETE);
-		producer.produce(thisRequest);
-		return Constants.REQUEST_QUEUED_MESSAGE;
+		return producer.produceMessage(thisRequest);
 
 	}
 
@@ -58,8 +49,7 @@ public class TrainerServiceImpl implements TrainerService {
 		User dummyUser = new Trainer(userName);
 		UserRequest thisRequest = new UserRequest(dummyUser);
 		thisRequest.setHowToAct(requestType.UPDATE);
-		producer.produce(thisRequest);
-		return Constants.REQUEST_QUEUED_MESSAGE;
+		return producer.produceMessage(thisRequest);
 	}
 
 }
--- src/main/java/com/qa/TrainerAPI/util/TrainerProducer.java
+++ src/main/java/com/qa/TrainerAPI/util/TrainerProducer.java
@@ -1,7 +1,10 @@
 package com.qa.TrainerAPI.util;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.JmsMessagingTemplate;
 import org.springframework.stereotype.Component;
 
 import com.qa.persistence.domain.Trainer;
@@ -11,21 +14,17 @@ import com.qa.persistence.domain.UserRequest;
 public class TrainerProducer {
 
 	@Autowired
-	private JmsTemplate jmsTemplate;
+	private JmsMessagingTemplate jmsTemplate;
 
-	public String produce(Trainer trainer) {
-		jmsTemplate.convertAndSend(Constants.INCOMING_TRAINER_QUEUE_NAME, trainer);
-		return Constants.TRAINER_QUEUED_MESSAGE;
+	public Optional<Trainer> produceTrainer(UserRequest request) {
+		return jmsTemplate.convertSendAndReceive(Constants.INCOMING_TRAINER_QUEUE_NAME, request, Optional.class);
 	}
 
-	public String produce(Iterable<Trainer> trainers) {
-		jmsTemplate.convertAndSend(Constants.INCOMING_TRAINER_QUEUE_NAME, trainers);
-		return Constants.TRAINERS_QUEUED_MESSAGE;
+	public Iterable<Trainer> produceTrainers(UserRequest request) {
+		return jmsTemplate.convertSendAndReceive(Constants.INCOMING_TRAINER_QUEUE_NAME, request, List.class);
 	}
 
-	public String produce(UserRequest request) {
-		jmsTemplate.convertAndSend(Constants.INCOMING_TRAINER_QUEUE_NAME, request);
-		return Constants.REQUEST_QUEUED_MESSAGE;
-
+	public String produceMessage(UserRequest request) {
+		return jmsTemplate.convertSendAndReceive(Constants.INCOMING_TRAINER_QUEUE_NAME, request, String.class);
 	}
 }
